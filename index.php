e21a873960d747d9978dfa1d149cffe076420a9f
colesayer <colesayershapiro@gmail.com>
2018-03-12 15:30:11 -0400

    updated stying and fixed temp cache

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
-
 $url = 'http://rentcafe.com/rentcafeapi.aspx?requestType=apartmentavailability&APIToken=NDY5OTI%3d-XDY6KCjhwhg%3d&propertyCode=p0155985';
 $cacheFile = 'cache' . DIRECTORY_SEPARATOR . md5($url);
 
-function runCache($url, $cacheFile){
+function runCache($url, $cacheFile, $tempCache = NULL){
   if(file_exists($cacheFile)){
     $cacheTime = filemtime($cacheFile);
     $now = time();
-    $tempCache = "";
 
       if(($now - $cacheTime) < 600){
         echo(file_get_contents($cacheFile));
@@ -16,7 +14,7 @@ function runCache($url, $cacheFile){
 
     $tempCache = file_get_contents($cacheFile);
     unlink($cacheFile);
-    runCache($url, $cacheFile);
+    runCache($url, $cacheFile, $tempCache);
   }
   else{
     $json = file_get_contents($url);
